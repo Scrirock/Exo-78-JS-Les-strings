b9a6120595648aaa4c46aeca35ba7c82e6744a83
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-14 09:31:05 +0100

    p

--- exercice1.js
+++ exercice1.js
@@ -2,15 +2,23 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let i = 0
+while (monTexte[i] !== "i"){
+    i++;
+}
+document.getElementById("position").innerHTML += i;
 
+let chaineMaj = monTexte.toUpperCase();
+document.getElementById("chaineMaj").innerHTML = chaineMaj;
 
-
-
-
-
-
+let lettre = 0;
+for (lettre of chaineMaj){
+    if (lettre === "A"){
+        alert("lettre A trouvé")
+    }
+}
